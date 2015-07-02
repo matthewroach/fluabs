df4892325b1482389872d64dbdef5628a76a0a50
Matthew Roach <matthew@matthewroach.me>
2015-07-03 00:01:38 +0100

    Refactor

--- src/fluabs.js
+++ src/fluabs.js
@@ -1,67 +1,71 @@
 (function( $ ) {
 
-  var methods = {
-    init : function( options ) {
-      return this.each(function() {
 
-        var s   = $.extend({}, $.fn.fluabs.defaults, options)
-          , el  = $(this)
-          , sEl = el.find(s.tabs);
 
-        $(s.content + ' div').hide();
+	var methods = {
+		init: function( _options ) {
+			return this.each(function() {
 
-        for ( var i=0; i < sEl.length; i++ ) {
+				var s = $.extend({}, $.fn.fluabs.defaults, _options);
+				var $el = $(this);
+				var sEl = $el.find(s.tabs);
 
-          if ( sEl[i].className === s.current ) {
+				$(s.content + ' > div').hide();
 
-            var openTab = $(sEl[i]).attr('href');
-            $(s.content + ' div[data-tab='+openTab+']').addClass(s.current).show();
+				for ( var i=0; i<sEl.length; i++ ) {
+					if ( sEl[i].className === s.current ) {
+						var openTab = sEl[i].hash;
+						$(s.content + ' > div[data-tab='+openTab+']').addClass(s.current).show();
+					}
+				}
 
-          }
+				$el.on('click.fluabs', s.tabs, function(e) {
+					e.preventDefault();
+					var $el = $(this);
+					var id = $el.prop('hash');
 
-        }
+					$el.removeClass(s.current);
+					$el.addClass(s.current);
 
-        el.on('click.fluabs', s.tabs, function(e) {
-          e.preventDefault();
+					$(s.content + ' > div:not([data-tab='+id+'])').removeClass(s.current).hide();
+					$(s.content + ' > div[data-tab='+id+']').fadeIn().addClass(s.current);
 
-          var el      = $(this)
-            , id      = el.prop('hash');
+				});
 
-          sEl.removeClass(s.current);
-          el.addClass(s.current);
+			});
+		},
 
-          $(s.content + ' div:not([data-tab='+id+'])').removeClass(s.current).hide();
-          $(s.content + ' div[data-tab='+id+']').fadeIn().addClass(s.current);
 
-        });
 
-      });
-    },
-    destroy : function( ) {
+		destroy: function( ) {
+			$(this).off('.fluabs');
+		}
+	};
 
-      $(this).off('.fluabs');
 
-    }
-  }
 
-  $.fn.fluabs = function( method ) {
+	$.fn.fluabs = function( _method ) {
 
-    // Method calling logic
-    if ( methods[method] ) {
-      return methods[ method ].apply( this, Array.prototype.slice.call( arguments, 1 ));
-    } else if ( typeof method === 'object' || ! method ) {
-      return methods.init.apply( this, arguments );
-    } else {
-      $.error( 'Method ' +  method + ' does not exist on jQuery.fluabs' );
-    }
+		// Method calling logic
+		if ( methods[_method] ) {
+			return methods[_method].apply( this, Array.prototype.slice.call( arguments, 1 ));
+		} else if ( typeof _method === 'object' || ! _method ) {
+			return methods.init.apply( this, arguments );
+		} else {
+			$.error( 'Method ' +  _method + ' does not exist on jQuery.fluabs' );
+		}
 
-  }
+	}
 
-  //default settings
-  $.fn.fluabs.defaults = {
-    tabs        : 'li a',
-    current     : 'current',
-    content     : '.tabcontent'
-  }
+
+
+	// default settings
+	$.fn.fluabs.defaults = {
+		tabs: 'li a',
+		current: 'current',
+		content: '.tabcontent'
+	};
+
+
 
 })(jQuery);
